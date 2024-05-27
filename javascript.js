ca672ec40ebe167a8b3506b058ab2b7e0a8f0f34
HemalathaBabu5056 <hemakoushi.babu@gmail.com>
2024-05-27 16:20:55 +0530

    js file updated

--- javascript.js
+++ javascript.js
@@ -31,3 +31,9 @@ const questions = [
   },
 ];
 
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score
